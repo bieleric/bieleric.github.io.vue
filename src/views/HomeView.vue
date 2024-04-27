6d78bb4107bbf08d82c20f67390df8422f135e4f
bieleric <biele.eric@gmail.com>
2024-04-27 21:59:51 +0200

    [ADD] social to second page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
   import { useNavigationStore } from "@/stores/navigationStore";
   import { ref } from "vue";
   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-  import { faStar } from '@fortawesome/free-solid-svg-icons';
+  import { faLinkedin, faSquareGithub, faSquareYoutube } from "@fortawesome/free-brands-svg-icons";
 
   const navigationStore = useNavigationStore();
 
@@ -17,10 +17,12 @@
     const pages = [page1, page2, page3, page4];
 
     pages.forEach(page => {
-      let rect = page.value.getBoundingClientRect();
+      if(page.value) {
+        let rect = page.value.getBoundingClientRect();
 
-      if(rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || el.clientHeight) && rect.right <= (window.innerWidth || el.clientWidth)) {
-        navigationStore.setCurrentPage(page.value.id)
+        if(rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || el.clientHeight) && rect.right <= (window.innerWidth || el.clientWidth)) {
+          navigationStore.setCurrentPage(page.value.id)
+        }
       }
     });
   }
@@ -36,6 +38,11 @@
     <div id="2" ref="page2" class="section h-dvh flex flex-col justify-center items-center">
       <p class="md:w-1/2 w-3/4 text-3xl mb-4">Welcome to my corner of the web!</p>
       <p class="md:w-1/2 w-3/4 font-thin text-xl">I'm Eric, an enthusiastic computer scientist based in Germany. With a passion for Web Development, Self-Sovereign Identity and Web3-Technologies, I strive to create cool and interesting projects to make people's live easier. Through this platform, I aim to showcase my journey and projects. I'm excited to share my story and collaborate on exciting opportunities.</p>
+      <div class="flex justify-evenly md:w-1/2 w-3/4 font-thin mt-8 text-3xl">
+        <a href="https://linkedin.com/in/eric-biele-ba9163185" target="_blank"><FontAwesomeIcon :icon="faLinkedin" /></a>
+        <a href="https://github.com/bieleric" target="_blank"><FontAwesomeIcon :icon="faSquareGithub" /></a>
+        <a href="#"><FontAwesomeIcon :icon="faSquareYoutube" /></a>
+      </div>
     </div>
 
     <div id="3" ref="page3" class="section h-dvh flex flex-col justify-center items-center">
